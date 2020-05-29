c0175847115367350a42687e5b5e6b740e687537
Helena Paixão <hp.helenapaixao@gmail.com>
2020-05-28 22:45:57 -0400

    configurando

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, {useEffect,useState} from "react";
+import api from './services/api';
 
 import {
   SafeAreaView,
@@ -11,6 +12,14 @@ import {
 } from "react-native";
 
 export default function App() {
+  const [repositories,setRepositories] =  useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+
+   setRepositories(response.data);
+    });
+  })
   async function handleLikeRepository(id) {
     // Implement "Like Repository" functionality
   }
@@ -20,35 +29,7 @@ export default function App() {
       <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
       <SafeAreaView style={styles.container}>
         <View style={styles.repositoryContainer}>
-          <Text style={styles.repository}>Repository 1</Text>
-
-          <View style={styles.techsContainer}>
-            <Text style={styles.tech}>
-              ReactJS
-            </Text>
-            <Text style={styles.tech}>
-              Node.js
-            </Text>
-          </View>
-
-          <View style={styles.likesContainer}>
-            <Text
-              style={styles.likeText}
-              // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
-              testID={`repository-likes-1`}
-            >
-              3 curtidas
-            </Text>
-          </View>
-
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => handleLikeRepository(1)}
-            // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
-            testID={`like-button-1`}
-          >
-            <Text style={styles.buttonText}>Curtir</Text>
-          </TouchableOpacity>
+  {/* {setRepositories.map(repository => <Text key={repository.id}>{repository.title}</Text>)} */}
         </View>
       </SafeAreaView>
     </>
